fa7b99660cf87a2d4732d6faf54e041b3b6b6764
Fung Hei Man <94099670+fung-hei-man@users.noreply.github.com>
2022-04-27 16:45:38 +0800

    feat: calculate accuracy

--- main.py
+++ main.py
@@ -22,6 +22,10 @@ def reshape_image(img):
     return shape, reshape
 
 
+def convert_to_1d_arr(img):
+    return img.reshape((-1, 3))[:, 0]
+
+
 def load_model(suffix):
     global model1, model2
     try:
@@ -36,21 +40,29 @@ def load_model(suffix):
 
 if __name__ == '__main__':
     for g_num in np.arange(MIN_G_NUM, MAX_G_NUM, step=G_NUM_STEP):
-        print(f">>> Number of gaussians: {g_num}")
+        img1_name = f'{INPUT_PATH}1.jpg'
+        img2_name = f'{INPUT_PATH}2.jpg'
+
         load_model(g_num)
 
-        img1_shape, img1 = reshape_image(io.imread(f"{INPUT_PATH}1.jpg"))
-        img2_shape, img2 = reshape_image(io.imread(f"{INPUT_PATH}2.jpg"))
+        img1_shape, img1 = reshape_image(io.imread(img1_name))
+        img2_shape, img2 = reshape_image(io.imread(img2_name))
 
+        truth1 = convert_to_1d_arr(io.imread(f'{INPUT_PATH}1_mask.png'))
+        truth2 = convert_to_1d_arr(io.imread(f'{INPUT_PATH}2_mask.png'))
+
+        print(f">>> Image: {img1_name}, Model: M1, Mixture#: {g_num}")
         p11 = predict.predict_image(model1, img1, img1_shape[:2], 'M1', 'soccer1', g_num)
+        predict.compare_result(p11, truth1, 'M1', g_num, 'soccer1')
+
+        print(f">>> Image: {img1_name}, Model: M2, Mixture#: {g_num}")
         p21 = predict.predict_image(model2, img1, img1_shape[:2], 'M2', 'soccer1', g_num)
+        predict.compare_result(p21, truth1, 'M2', g_num, 'soccer1')
+
+        print(f">>> Image: {img2_name}, Model: M1, Mixture#: {g_num}")
         p12 = predict.predict_image(model1, img2, img2_shape[:2], 'M1', 'soccer2', g_num)
+        predict.compare_result(p12, truth2, 'M1', g_num, 'soccer2')
+
+        print(f">>> Image: {img2_name}, Model: M2, Mixture#: {g_num}")
         p22 = predict.predict_image(model2, img2, img2_shape[:2], 'M2', 'soccer2', g_num)
-
-        _, truth1 = reshape_image(io.imread(f'{INPUT_PATH}1_mask.png'))
-        _, truth2 = reshape_image(io.imread(f'{INPUT_PATH}2_mask.png'))
-
-        predict.compare_result(p11, truth1, 'M1', 'soccer1')
-        predict.compare_result(p21, truth1, 'M2', 'soccer1')
-        predict.compare_result(p12, truth2, 'M1', 'soccer2')
-        predict.compare_result(p22, truth2, 'M2', 'soccer2')
+        predict.compare_result(p22, truth2, 'M2', g_num, 'soccer2')
--- predict.py
+++ predict.py
@@ -6,10 +6,9 @@ OUTPUT_PATH = 'output/segments/'
 
 def predict_image(model, image, image_shape, model_name, img_name, g_num):
     result = model.predict(image)
-    print(f"=== Predict {img_name} with {model_name} ({g_num} Gaussians) ===")
     result_img = result.reshape(image_shape)
 
-    plt.title(f"Predict {img_name} with {model_name} ({g_num} Gaussians)")
+    plt.title(f"Color Segment of {img_name} with {model_name} ({g_num} Gaussians)")
     plt.imshow(result_img), plt.axis('off')
 
     plt.savefig(f"{OUTPUT_PATH}{img_name}_{model_name}_{g_num}.png".lower())
@@ -24,24 +23,47 @@ def get_playf_gaussian_labels(prediction, ground_truth):
     pred_bins = np.bincount(prediction)
     # len(pred_bins) = g_nums
     playf_bins = np.zeros(len(pred_bins))
-    one_d_gt = ground_truth[:, 0]
 
-    for i in range(len(one_d_gt)):
-        if one_d_gt[i] == 255:
+    for i in range(len(ground_truth)):
+        if ground_truth[i] == 255:
             playf_bins[prediction[i]] += 1
 
-    print('pred_bins')
+    print('pred_bins', end=': ')
     print(pred_bins)
-    print('playf_bins')
+    print('playf_bins', end=': ')
     print(playf_bins)
 
-    gmm_is_gaussion = [playf_bins[i]/pred_bins[i] > 0.5 for i in range(len(playf_bins))]
+    gmm_is_gaussian = [playf_bins[i]/pred_bins[i] > 0.7 if playf_bins[i] != 0 and pred_bins[i] != 0 else False for i in range(len(playf_bins))]
     gmm_index = np.array(range(len(pred_bins)))
+    return gmm_index[gmm_is_gaussian]
 
-    return gmm_index[gmm_is_gaussion]
+
+# convert to 1 if predicted label is in gmm_is_gaussian
+def convert_pred_to_binary(prediction, gmm_is_gaussian):
+    for i in range(len(prediction)):
+        if prediction[i] in gmm_is_gaussian:
+            prediction[i] = 1
+        else:
+            prediction[i] = 0
+
+    return prediction
 
 
-def compare_result(prediction, ground_truth, model_name, img_name):
-    gmm_is_gaussion = get_playf_gaussian_labels(prediction, ground_truth)
-    print('gmm_is_gaussion')
-    print(gmm_is_gaussion)
+def compare_result(prediction, ground_truth, model_name, g_num, img_name):
+    gmm_is_gaussian = get_playf_gaussian_labels(prediction, ground_truth)
+    print('gmm_is_gaussian', end=': ')
+    print(gmm_is_gaussian)
+    binary_prediction = convert_pred_to_binary(prediction, gmm_is_gaussian)
+    correct_num = 0
+
+    for i in range(len(ground_truth)):
+        if ground_truth[i] == 255 and binary_prediction[i] == 1:
+            correct_num += 1
+        # both 0
+        elif ground_truth[i] == binary_prediction[i]:
+            correct_num += 1
+
+    print(f'Number of correct pixel {correct_num}, total pixel {len(ground_truth)}')
+
+    accuracy = correct_num / len(ground_truth)
+    print(f'* Accuracy of {img_name} with {model_name} ({g_num} Gaussian): {accuracy}')
